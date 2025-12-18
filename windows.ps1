1f9c49f4bee98bf068c09bef59eb68ae314bc538
Pedro Lucena <pedrolucena.dev@gmail.com>
2025-12-17 21:43:09 -0300

    feat(windows): add dev apps install

--- windows.ps1
+++ windows.ps1
@@ -3,16 +3,29 @@
 ###━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 function Install-Web-Browsers {
-	winget settings --enable InstallerHashOverride
-
 	winget install --id Google.Chrome
 	winget install Mozilla.Firefox
+}
 
-	winget settings --disable InstallerHashOverride
+function Install-Dev-Apps {
+    winget install --id Git.Git
+    
+    winget install --id Microsoft.VisualStudioCode
+    
+    winget install --id CoreyButler.NVMforWindows
+
+    winget install --id Docker.DockerDesktop
+    winget install --id Amazon.AWSCLI
+    winget install --id Hashicorp.Terraform
 }
 
 ###━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 ### EXECUTION
 ###━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
+winget settings --enable InstallerHashOverride
+
 Install-Web-Browsers
+Install-Dev-Apps
+
+winget settings --disable InstallerHashOverride
