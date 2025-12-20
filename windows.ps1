7344f501e829b18ecc94ec54ec442ef90fac2459
Pedro Lucena <pedrolucena.dev@gmail.com>
2025-12-20 13:40:39 -0300

    feat(windows): add "config windows hello" to manual

--- windows.ps1
+++ windows.ps1
@@ -151,6 +151,7 @@ function Manual {
     # [] Add e-mails to web browser
     # [] Config AWS CLI profiles
     # [] Config SSH to Github connection (https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#generating-a-new-ssh-key)
+    # Config Windows Hello
 }
 
 function Install-Package-Managers {
